1de0a281d8c0798210c061252bfa357595a51269
avery13 <34750921+avery13@users.noreply.github.com>
2018-05-18 13:35:07 -0400

    Update coords-if-statements.py
    ###### separates quadrants

--- coords-if-statements.py
+++ coords-if-statements.py
@@ -1,1 +1,252 @@
+place = [0,0]
+if place == [0,0]:
+    print("hi")
+elif place == [0,1]:
+    print("hi")
+elif place == [0,2]:
+    print("hi")
+elif place == [0,3]:
+    print("hi")
+elif place == [0,4]:
+    print("hi")
+elif place == [0,5]:
+    print("hi")
+elif place == [1,0]:
+    print("hi")
+elif place == [1,1]:
+    print("hi")
+elif place == [1,2]:
+    print("hi")
+elif place == [1,3]:
+    print("hi")
+elif place == [1,4]:
+    print ("hi")
+elif place == [1,5]:
+    print("hi")
+elif place == [2,0]:
+    print("hi")
+elif place == [2,1]:
+    print("hi")
+elif place == [2,2]:
+    print("hi")
+elif place == [2,3]:
+    print("hi")
+elif place == [2,4]:
+    print("hi")
+elif place == [2,5]:
+    print("hi")
+elif place == [3,0]:
+    print("hi")
+elif place == [3,1]:
+    print("hi")
+elif place == [3,2]:
+    print("hi")
+elif place == [3,3]:    
+    print("hi")
+elif place == [3,4]:
+    print("hi")
+elif place == [3,5]:
+    print("hi")
+elif place == [4,0]:
+    print("hi")
+elif place == [4,1]:
+    print("hi")
+elif place == [4,2]:
+    print("hi")
+elif place == [4,3]:
+    print("hi")
+elif place == [4,4]:
+    print("hi")    
+elif place == [4,5]:
+    print("hi")
+elif place == [5,0]:
+    print("hi")
+elif place == [5,1]:
+    print("hi")
+elif place == [5,2]:
+    print("hi")
+elif place == [5,3]:    
+    print("hi")
+elif place == [5,4]:
+    print("hi")
+elif place == [5,5]:
+    print("hi")
+    
+#################################################################################################################################################################################################
 
+if place == [-1,0]:
+        print("hi")
+elif place == [-1,1]:
+    print("hi")
+elif place == [-1,2]:
+    print("hi")
+elif place == [-1,3]:
+    print("hi")
+elif place == [-1,4]:
+    print ("hi")
+elif place == [-1,5]:
+    print("hi")
+elif place == [-2,0]:
+    print("hi")
+elif place == [-2,1]:
+    print("hi")
+elif place == [-2,2]:
+    print("hi")
+elif place == [-2,3]:
+    print("hi")
+elif place == [-2,4]:
+    print("hi")
+elif place == [-2,5]:
+    print("hi")
+elif place == [-3,0]:
+    print("hi")
+elif place == [-3,1]:
+    print("hi")
+elif place == [-3,2]:
+    print("hi")
+elif place == [-3,3]:    
+    print("hi")
+elif place == [-3,4]:
+    print("hi")
+elif place == [-3,5]:
+    print("hi")
+elif place == [-4,0]:
+    print("hi")
+elif place == [-4,1]:
+    print("hi")
+elif place == [-4,2]:
+    print("hi")
+elif place == [-4,3]:
+    print("hi")
+elif place == [-4,4]:
+    print("hi")    
+elif place == [-4,5]:
+    print("hi")
+elif place == [-5,0]:
+    print("hi")
+elif place == [-5,1]:
+    print("hi")
+elif place == [-5,2]:
+    print("hi")
+elif place == [-5,3]:    
+    print("hi")
+elif place == [-5,4]:
+    print("hi")
+elif place == [-5,5]:
+    print("hi")
+    
+#################################################################################################################################################################################################
+
+
+if place == [0,-1]:
+    print("hi")
+elif place == [0,-2]:
+    print("hi")
+elif place == [0,-3]:
+    print("hi")
+elif place == [0,-4]:
+    print("hi")
+elif place == [0,-5]:
+    print("hi")
+elif place == [-1,-1]:
+    print("hi")
+elif place == [-1,-2]:
+    print("hi")
+elif place == [-1,-3]:
+    print("hi")
+elif place == [-1,-4]:
+    print ("hi")
+elif place == [-1,-5]:
+    print("hi")
+elif place == [-2,-1]:
+    print("hi")
+elif place == [-2,-2]:
+    print("hi")
+elif place == [-2,-3]:
+    print("hi")
+elif place == [-2,-4]:
+    print("hi")
+elif place == [-2,-5]:
+    print("hi")
+elif place == [-3,-1]:
+    print("hi")
+elif place == [-3,-2]:
+    print("hi")
+elif place == [-3,-3]:    
+    print("hi")
+elif place == [-3,-4]:
+    print("hi")
+elif place == [-3,-5]:
+    print("hi")
+elif place == [-4,-1]:
+    print("hi")
+elif place == [-4,-2]:
+    print("hi")
+elif place == [-4,-3]:
+    print("hi")
+elif place == [-4,-4]:
+    print("hi")    
+elif place == [-4,-5]:
+    print("hi")
+elif place == [-5,-1]:
+    print("hi")
+elif place == [-5,-2]:
+    print("hi")
+elif place == [-5,-3]:    
+    print("hi")
+elif place == [-5,-4]:
+    print("hi")
+elif place == [-5,-5]:
+    print("hi")
+    
+##################################################################################################################################################################################################
+if place == [1,-1]:
+    print("hi")
+elif place == [1,-2]:
+    print("hi")
+elif place == [1,-3]:
+    print("hi")
+elif place == [1,-4]:
+    print ("hi")
+elif place == [1,-5]:
+    print("hi")
+elif place == [2,-1]:
+    print("hi")
+elif place == [2,-2]:
+    print("hi")
+elif place == [2,-3]:
+    print("hi")
+elif place == [2,-4]:
+    print("hi")
+elif place == [2,-5]:
+    print("hi")
+elif place == [3,-1]:
+    print("hi")
+elif place == [3,-2]:
+    print("hi")
+elif place == [3,-3]:    
+    print("hi")
+elif place == [3,-4]:
+    print("hi")
+elif place == [3,-5]:
+    print("hi")
+elif place == [4,-1]:
+    print("hi")
+elif place == [4,-2]:
+    print("hi")
+elif place == [4,-3]:
+    print("hi")
+elif place == [4,-4]:
+    print("hi")    
+elif place == [4,-5]:
+    print("hi")
+elif place == [5,-1]:
+    print("hi")
+elif place == [5,-2]:
+    print("hi")
+elif place == [5,-3]:    
+    print("hi")
+elif place == [5,-4]:
+    print("hi")
+elif place == [5,-5]:
+    print("hi")
